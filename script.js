c1ed2247a2dbf805db6212eb577063f8ef2a6a0e
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2025-07-17 00:39:32 +0100

    add tabbed component

--- script.js
+++ script.js
@@ -73,3 +73,33 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+// event delegation
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause (to prevent null as result when you click outside the button)
+  if (!clicked) return;
+
+  // Remove active class
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  console.log(tabsContent);
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
