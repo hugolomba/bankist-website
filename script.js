c21fd2bb7e8c7980dbc26fe50db6ff4183164b73
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2025-07-16 20:51:09 +0100

    add event delegation to put smooth scrolling in all nav elements

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
+// Selectors
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,17 +34,12 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-////
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+// Scrolling
 
 btnScrollTo.addEventListener('click', function (e) {
   e.preventDefault();
 
   const s1coords = section1.getBoundingClientRect();
-
-  // Scrolling;
 
   // old way to do with calculations
   // window.scrollTo({
@@ -51,4 +50,26 @@ btnScrollTo.addEventListener('click', function (e) {
 
   // modern way to do
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// Page navigation
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//
+//   });
+// });
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  // console.log(e.target.classList);
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    e.preventDefault();
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
